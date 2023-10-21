19fd4fc537bd2bfb548b31df2e3cd21e91e3b2a7
HimalayBhalala <bhimalaybhalala96@gmail.com>
2023-10-21 09:43:46 +0530

    Deployment

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id':1,
+        'title':"Data Analyst",
+        'location':"Bengaluru,Bharat",
+        'salary':"Rs.10,00,000"
+    },
+    {
+        'id':2,
+        'title':"Data Mining",
+        'location':"Bengaluru,Bharat",
+        'salary':"Rs.20,00,000"
+    },
+    {
+        'id':3,
+        'title':"Java",
+        'location':"Pune,Bharat",
+        'salary':"Rs.30,00,000"
+    },
+    {
+        'id':4,
+        'title':"PHP Devloper",
+        'location':"Pune,Bharat",
+        'salary':"Rs.5,00,000"
+    },
+    {
+        'id':5,
+        'title':"Django",
+        'location':"Kerala,Bharat",
+    },
+    {
+        'id':6,
+        'title':"Spring Boot",
+        'location':"Bengaluru,Bharat",
+        'salary':"Rs.20,00,000"
+    }
+]
+
 @app.route("/")
 def hello_World():
-    return render_template("home.html")
+    return render_template("home.html",jobs=JOBS,about="About")
 
-print(__name__)
-
+@app.route("/api/json")
+def jsonapp():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
